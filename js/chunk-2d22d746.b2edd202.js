a24bdfec62c5f4d55bf1219bb28061d471fa6c96
nasimulhasandeep <nasimulhasan513@gmail.com>
2021-10-11 00:13:41 +0600

    updated

---
+++ js/chunk-2d22d746.b2edd202.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,c){"use strict";c.r(e);var t=c("7a23");function o(n,e,c,o,r,u){return Object(t["openBlock"])(),Object(t["createElementBlock"])("div")}var r={setup:function(){}},u=c("6b0d"),d=c.n(u);const a=d()(r,[["render",o]]);e["default"]=a}}]);
+//# sourceMappingURL=chunk-2d22d746.b2edd202.js.map
